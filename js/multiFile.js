d8092ca02dcf16d731f9b9e1e2248ecc8277d01b
keganokami <400800m@gmail.com>
2019-06-24 23:01:56 +0900

    js delete

--- js/multiFile.js
+++
@@ -1,9 +0,0 @@
-function printfile() {
-  var fileInput = document.getElementById("fileInput").files;
-  var list = "";
-  for (var i = 0; i < fileInput.length; i++) {
-    list += fileInput[i].name + "<br>";
-  }
-  document.getElementById("result").innerHTML = list;
-}
-
