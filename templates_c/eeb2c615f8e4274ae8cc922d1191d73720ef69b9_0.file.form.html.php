b672e097beaafaaa8f0dedf4f53bde1ec1614fb6
keganokami <400800m@gmail.com>
2019-07-08 23:12:33 +0900

    テキスト　追加

--- templates_c/eeb2c615f8e4274ae8cc922d1191d73720ef69b9_0.file.form.html.php
+++ templates_c/eeb2c615f8e4274ae8cc922d1191d73720ef69b9_0.file.form.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2019-06-24 13:10:33
+/* Smarty version 3.1.33, created on 2019-07-08 13:19:42
   from 'C:\Apache24\htdocs\rebirth_climbing\templates\htmlContents\form.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5d10cbc99802d7_03074896',
+  'unifunc' => 'content_5d2342ee81ecc4_10608707',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'eeb2c615f8e4274ae8cc922d1191d73720ef69b9' => 
     array (
       0 => 'C:\\Apache24\\htdocs\\rebirth_climbing\\templates\\htmlContents\\form.html',
-      1 => 1561381602,
+      1 => 1561726236,
       2 => 'file',
     ),
   ),
@@ -20,17 +20,29 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5d10cbc99802d7_03074896 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5d2342ee81ecc4_10608707 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div id="q_form">
   <div class="q_email_form">
     <div>
       <h2 class="q_title">お問い合わせ</h2>
     </div>
     <div>
-      <form action="#" method="post" name="form" onsubmit="return validate()">
+      <form
+        action="./index.php"
+        method="post"
+        name="form"
+        onsubmit="return validate()"
+      >
         <p>
-          お問い合わせ内容をご入力の上、「確認画面へ」ボタンをクリックしてください。
+          お問い合わせ内容をご入力の上、「確認画面へ」ボタンをクリックしてください。<br />
+          <span class="tip"
+            >確認メールの受信をもって送信の完了となります。</span
+          >
         </p>
+        <p class="mail_result"><?php echo $_smarty_tpl->tpl_vars['success_send_mail']->value;?>
+</p>
+        <p class="mail_result"><?php echo $_smarty_tpl->tpl_vars['error_send_mail']->value;?>
+</p>
         <div>
           <div class="form">
             <label>お名前<span>必須</span></label>
@@ -39,10 +51,11 @@ function content_5d10cbc99802d7_03074896 (Smarty_Internal_Template $_smarty_tpl)
               name="name"
               placeholder="例）山田太郎"
               value=""
+              required
             />
           </div>
           <div class="form">
-            <label>ふりがな<span>必須</span></label>
+            <label>ふりがな</label>
             <input
               type="text"
               name="furigana"
@@ -57,11 +70,12 @@ function content_5d10cbc99802d7_03074896 (Smarty_Internal_Template $_smarty_tpl)
               name="email"
               placeholder="例）guest@example.com"
               value=""
+              required
             />
           </div>
           <div class="form">
             <label>お問い合わせ項目<span>必須</span></label>
-            <select name="item">
+            <select name="item" required>
               <option value="">選択してください</option>
               <option value="体験入部希望">体験入部希望</option>
               <option value="質問・疑問">ご質問・疑問</option>
@@ -71,12 +85,13 @@ function content_5d10cbc99802d7_03074896 (Smarty_Internal_Template $_smarty_tpl)
             <textarea
               name="content"
               rows="5"
-              placeholder="お問合せ内容を入力"
+              placeholder="お問合せ内容を入力　必須"
+              required
             ></textarea>
           </div>
         </div>
         <div class="submit_form">
-          <button type="submit">確認画面へ</button>
+          <button type="submit">送信する</button>
         </div>
       </form>
     </div>
